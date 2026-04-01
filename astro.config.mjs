071192cb5fb634a61f05d9a062fa1ca179c473d9
Fábio M.R. Barbosa <fabio@fabiobarbosa.com>
2026-04-01 16:17:21 +0200

    Try to fix Turnstile

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astrofabio.com',
   adapter: node({ mode: 'standalone' }),
   output: 'server',
+  security: {
+    checkOrigin: false,
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pt'],
